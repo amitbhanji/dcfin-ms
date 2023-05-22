e3c2a9351785d329b544cc048aee52e3787bb34b
smitaprabhu92 <smitaprabhu92@gmail.com>
2023-05-22 21:03:28 +0530

    a
    a

---
+++ spring-security/src/test/java/com/example/spriing/security/SpringSecurityApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.spriing.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	private 
+	@Test
+	void contextLoads() {
+	}
+
+	
+	
+	  
+}
