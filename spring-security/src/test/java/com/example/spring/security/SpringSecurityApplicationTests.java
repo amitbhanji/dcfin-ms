d31858204f42dd9208d11e3508ceb029b297bf9c
smitaprabhu92 <smitaprabhu92@gmail.com>
2023-05-23 22:11:12 +0530

    a
    a

--- spring-security/src/test/java/com/example/spring/security/SpringSecurityApplicationTests.java
+++ spring-security/src/test/java/com/example/spring/security/SpringSecurityApplicationTests.java
@@ -1,13 +1,34 @@
 package com.example.spring.security;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.repository.data.User;
+import com.repository.service.UserJpaRepository;
+
+@DataJpaTest
+@AutoConfigureTestDatabase
 @SpringBootTest
 class SpringSecurityApplicationTests {
 
-	@Test
+	@Autowired
+	private UserJpaRepository userJpaRepository;
+	//@Test
 	void contextLoads() {
 	}
 
+	@Test
+	public void createUser()
+	{
+		User user =new User();
+		user.setUserfname("aboli");
+		user.setUsername("ab");
+		user.setUserEmail("aboli123@gmail.com");
+		user.setPassword("aboli");
+		
+		userJpaRepository.save(user);
+	}
 }
